9b7752026c4f17c9122896401a17e9a1a9eea043
Tycho Doorman <tycho26@kpnmail.nl>
2021-01-02 16:22:42 +0100

    Small tweaks (#7)
    * Corrected issue with folder paths.
    
    * Added more prints for feedback.

--- main.py
+++ main.py
@@ -3,17 +3,21 @@ from PIL import Image
 
 base_folder = Path(__file__).parent.absolute()
 root_folder = Path(Path.joinpath(base_folder,"test_data"))
-pc_folder = Path(Path.joinpath(base_folder,"test_data/PC"))
-phone_folder = Path(Path.joinpath(base_folder,"test_data/Phone"))
-other_folder = Path(Path.joinpath(base_folder,"test_data/Other"))
+pc_folder = Path(Path.joinpath(root_folder,"PC"))
+phone_folder = Path(Path.joinpath(root_folder,"Phone"))
+other_folder = Path(Path.joinpath(root_folder,"Other"))
 
 if not root_folder.exists():
+    print(root_folder,"Does not exist. Creating now...")
     root_folder.mkdir()
 if not pc_folder.exists():
+    print(pc_folder,"Does not exist. Creating now...")
     pc_folder.mkdir()
 if not phone_folder.exists():
+    print(phone_folder,"Does not exist. Creating now...")
     phone_folder.mkdir()
 if not other_folder.exists():
+    print(other_folder,"Does not exist. Creating now...")
     other_folder.mkdir()
 
 wallpapers = list(root_folder.glob("*.jpg"))
