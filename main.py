cbc7bf6c88d131fc623335ca03ce4597aa834955
Tycho Doorman <tycho26@kpnmail.nl>
2021-01-03 02:58:44 +0100

    Dev (#10)
    * WS-1: Read contents of root folder and filter images (#5)
    
    * Added test_data folder to .gitignore.
    
    * Filters through root folder and gets all image files.
    
    * Filter wallpapers based on image resolution. (#6)
    
    * Small tweaks (#7)
    
    * Corrected issue with folder paths.
    
    * Added more prints for feedback.
    
    * Wallpapers now get moved to the correct folder. Also added datetime to prints for logging. (#8)
    
    * WS-4: Make folder paths changeable (#9)
    
    * Added settings.json file to .gitignore.
    
    * Added settings example file.
    
    * Settings now get read from settings.json.

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+import shutil
+from PIL import Image
+from pathlib import Path
+from datetime import datetime
+
+settings_file = open("settings.json","r").read()
+settings = json.loads(settings_file)
+
+base_folder = Path(__file__).parent.absolute()
+root_folder = Path(Path.joinpath(base_folder,settings['folders']['root']))
+pc_folder = Path(Path.joinpath(root_folder,settings['folders']['pc']))
+phone_folder = Path(Path.joinpath(root_folder,settings['folders']['phone']))
+other_folder = Path(Path.joinpath(root_folder,settings['folders']['other']))
+
+if not root_folder.exists():
+    print("[",datetime.now(),"]",root_folder,"does not exist. Creating now...")
+    root_folder.mkdir()
+if not pc_folder.exists():
+    print("[",datetime.now(),"]",pc_folder,"does not exist. Creating now...")
+    pc_folder.mkdir()
+if not phone_folder.exists():
+    print("[",datetime.now(),"]",phone_folder,"does not exist. Creating now...")
+    phone_folder.mkdir()
+if not other_folder.exists():
+    print("[",datetime.now(),"]",other_folder,"does not exist. Creating now...")
+    other_folder.mkdir()
+
+start_time = datetime.timestamp(datetime.now())
+
+wallpapers = list(root_folder.glob("*.jpg"))
+wallpapers.extend(root_folder.glob("*.jpeg"))
+wallpapers.extend(root_folder.glob("*.png"))
+
+for wallpaper in wallpapers:
+    img = Image.open(wallpaper)
+    if img.size[0] < img.size[1]:
+        print("[",datetime.now(),"]","Moving",wallpaper,"to Phone")
+        shutil.move(wallpaper,phone_folder)
+    elif img.size[0] > img.size[1]:
+        print("[",datetime.now(),"]","Moving",wallpaper,"to PC")
+        shutil.move(wallpaper,pc_folder)
+    else:
+        print("[",datetime.now(),"]","Moving",wallpaper,"to Other")
+        shutil.move(wallpaper,other_folder)
+
+end_time = datetime.timestamp(datetime.now())
+print("[",datetime.now(),"]","Done in",round(end_time - start_time,2),"seconds.")
